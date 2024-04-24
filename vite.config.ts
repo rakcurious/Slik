0b670494283063de91912c1081e12aa2dad97e6b
rakcurious <rakcurious@gmail.com>
2024-04-24 19:53:01 +0530

    minor pwa improvements

--- vite.config.ts
+++ vite.config.ts
@@ -15,17 +15,23 @@ export default defineConfig({
         name: 'Slik',
         short_name: 'Slik',
         description: 'curated drip from your favorite fashion brands',
-        theme_color: '#f3e8ff',
+        theme_color: '#F3E8FF',
+        background_color: '#F3E8FF',
+        start_url: 'https://beslik.in/',
+        display: 'fullscreen',
+        prefer_related_applications: false,
         icons: [
           {
             src: 'pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png',
+            purpose: 'maskable'
           },
           {
             src: 'pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
+            purpose: 'maskable'
           },
         ],
       },
