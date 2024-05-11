8b64f89b7402bfd313ad008d89fa61fd50960ac4
rakcurious <rakcurious@gmail.com>
2024-05-11 16:49:24 +0530

    social media previews for products not working

--- src/components/Meta.tsx
+++ src/components/Meta.tsx
@@ -3,25 +3,25 @@ import { Helmet } from 'react-helmet';
 
 interface MetaProps {
   title: string;
-  description?: string;
-  image?: string;
+  description: string;
+  image: string;
   url: string;
 }
 
-const Meta: React.FC<MetaProps> = ({ title, url }) => (
+const Meta: React.FC<MetaProps> = ({ title, description, image, url }) => (
   <Helmet>
-    <title>Product</title>
-    <meta name="description" content='Visit Slik for Product Info' data-react-helmet="true" />
-    <meta property="og:title" content="Product" data-react-helmet="true" />
-    <meta property="og:description" content='Visit Slik for Product Info' data-react-helmet="true" />
-    <meta property="og:url" content={url} data-react-helmet="true" />
-    <meta property="og:image" content="https://res.cloudinary.com/dnhz5reqf/image/upload/v1714373699/slik/level_up_your_fashion_game_pkf4ye.webp" data-react-helmet="true"/>
-    <meta property="og:type" content="website" data-react-helmet="true" />
-    <meta property="og:site_name" content="Slik" data-react-helmet="true" />
-    <meta name="twitter:card" content="summary_large_image" data-react-helmet="true" />
-    <meta name="twitter:title" content='Product' data-react-helmet="true" />
-   <meta name="twitter:image" content="https://res.cloudinary.com/dnhz5reqf/image/upload/v1714373699/slik/level_up_your_fashion_game_pkf4ye.webp" data-react-helmet="true" />
-   <meta name="twitter:image:alt" content={title} data-react-helmet="true" />
+    <title>{title}</title>
+    <meta name="description" content={description} />
+    <meta property="og:title" content={title} />
+    <meta property="og:description" content={description} />
+    <meta property="og:url" content={url} />
+    <meta property="og:image" content={image} />
+    <meta property="og:type" content="website" />
+    <meta property="og:site_name" content="Slik" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:title" content={title} />
+    <meta name="twitter:image" content={image} />
+    <meta name="twitter:image:alt" content={title} />
   </Helmet>
 );
 
