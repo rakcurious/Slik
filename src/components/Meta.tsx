f3283c769cfbccfed1f7b2d9177e5e8fac6102e6
rakcurious <rakcurious@gmail.com>
2024-05-11 16:36:12 +0530

    still trying to add social media previews and react still sucks

--- src/components/Meta.tsx
+++ src/components/Meta.tsx
@@ -4,24 +4,24 @@ import { Helmet } from 'react-helmet';
 interface MetaProps {
   title: string;
   description: string;
-  image?: string;
+  image: string;
   url: string;
 }
 
 const Meta: React.FC<MetaProps> = ({ title, description, image, url }) => (
   <Helmet>
     <title>{title}</title>
-    <meta name="description" content={description} />
-    <meta property="og:title" content={title} />
-    <meta property="og:description" content={description} />
-    <meta property="og:url" content={url} />
-    <meta property="og:image" content={image} />
-    <meta property="og:type" content="website" />
-    <meta property="og:site_name" content="Slik" />
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:title" content={title} />
-   <meta name="twitter:image" content={image} />
-   <meta name="twitter:image:alt" content={title} />
+    <meta name="description" content={description} data-react-helmet="true" />
+    <meta property="og:title" content={title} data-react-helmet="true" />
+    <meta property="og:description" content={description} data-react-helmet="true" />
+    <meta property="og:url" content={url} data-react-helmet="true" />
+    <meta property="og:image" content={image} data-react-helmet="true"/>
+    <meta property="og:type" content="website" data-react-helmet="true" />
+    <meta property="og:site_name" content="Slik" data-react-helmet="true" />
+    <meta name="twitter:card" content="summary_large_image" data-react-helmet="true" />
+    <meta name="twitter:title" content={title} data-react-helmet="true" />
+   <meta name="twitter:image" content={image} data-react-helmet="true" />
+   <meta name="twitter:image:alt" content={title} data-react-helmet="true" />
   </Helmet>
 );
 
