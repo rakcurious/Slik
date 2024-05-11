671047b2142b98763031a88d95c7a163a76bbb5d
rakcurious <rakcurious@gmail.com>
2024-05-11 17:50:38 +0530

    still testing dynamic social media previews

--- src/pages/ProductInfo.tsx
+++ src/pages/ProductInfo.tsx
@@ -75,22 +75,22 @@ const ProductInfo: React.FC = () => {
     <>
     <Helmet>
     <title>Product</title>
-    <meta name="description" content={`${product?.brand} - ₹${product?.price.toLocaleString("en-IN")}`} />
-    <meta property="og:title" content={product?.title} />
-    <meta property="og:description" content={`${product?.brand} - ₹${product?.price.toLocaleString("en-IN")}`} />
+    <meta name="description" content='Product Details on Slik' data-react-helmet="true" />
+    <meta property="og:title" content='Product' />
+    <meta property="og:description" content='Product Details on Slik' data-react-helmet="true" />
     <meta property="og:url" content={window.location.href} />
     <meta property="og:image" content="https://res.cloudinary.com/dnhz5reqf/image/upload/v1714373699/slik/level_up_your_fashion_game_pkf4ye.webp" />
     <meta property="og:type" content="website" />
     <meta property="og:site_name" content="Slik" />
     <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:title" content={product?.title} />
+    <meta name="twitter:title" content='Product' />
     <meta name="twitter:image" content="https://res.cloudinary.com/dnhz5reqf/image/upload/v1714373699/slik/level_up_your_fashion_game_pkf4ye.webp" />
-    <meta name="twitter:image:alt" content={product?.title} />
+    <meta name="twitter:image:alt" content='Product' />
   </Helmet>
       <Navbar />
       {product ? (
         <>
-  <Helmet>
+  {/* <Helmet>
     <title>{product?.title}</title>
     <meta name="description" content={`${product?.brand} - ₹${product?.price.toLocaleString("en-IN")}`} />
     <meta property="og:title" content={product?.title} />
@@ -103,7 +103,7 @@ const ProductInfo: React.FC = () => {
     <meta name="twitter:title" content={product?.title} />
     <meta name="twitter:image" content={product?.images[0]} />
     <meta name="twitter:image:alt" content={product?.title} />
-  </Helmet>
+  </Helmet> */}
           <div className="hidden xl:flex h-auto overflow-hidden">
             <div className="w-2/3 overflow-y-auto flex flex-wrap p-4 justify-center scrollbar-none">
               {product?.images.map((image, index) => (
